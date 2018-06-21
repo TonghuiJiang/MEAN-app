d63d05d8e59fbb8f5d6a5688d087ea66d89dc6b0
TonghuiJiang <jiangt14@mcmaster.ca>
2018-06-20 21:44:26 -0400

    Getting Posts from Post-Create to Post-List

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Post } from '../post.model';
+import { PostsService} from '../posts.service';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -14,4 +15,6 @@ export class PostListComponent {
 
   // ];
   @Input () posts: Post[] = [];
+
+  constructor(public postsService: PostsService) {}
 }
