c997c03a55dc96e813e5d43b32e1f6ea4b8916e2
TonghuiJiang <jiangt14@mcmaster.ca>
2018-06-21 23:36:44 -0400

    Calling GET Post

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,14 +1,24 @@
 import { Post } from './post.model';
+import { Injectable } from '@angular/core';
+import { Subject} from 'rxjs';
 
+
+
+@Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
+  private psotsUpdated = new Subject<Post[]>();
 
-  getPost() {
+  getPosts() {
     return [...this.posts];
   }
 
+  getPostUpdateListener() {
+    return this.psotsUpdated.asObservable();
+  }
   addPost(title: string, content: string) {
     const post: Post = {title: title, content: content};
     this.posts.push(post);
+    this.psotsUpdated.next([...this.posts]);
   }
 }
