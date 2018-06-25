1308b41c8c5bf9c79921a40660900751c18e393f
TonghuiJiang <jiangt14@mcmaster.ca>
2018-06-24 21:56:33 -0400

    Using the Angular HTTP Client

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,4 +1,5 @@
 import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject} from 'rxjs';
 
@@ -9,15 +10,21 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+    .subscribe((postData) => {
+      this.posts = postData.posts;
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 
   getPostUpdateListener() {
     return this.postsUpdated.asObservable();
   }
   addPost(title: string, content: string) {
-    const post: Post = {title: title, content: content};
+    const post: Post = { id: null, title: title, content: content};
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
