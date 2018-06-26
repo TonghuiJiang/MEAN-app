86088061c21b2afa7bf48169cb7e15f94ca71e0c
TonghuiJiang <jiangt14@mcmaster.ca>
2018-06-25 23:43:02 -0400

    Understanding CORS

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,18 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-with, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods",
+  "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use((req, res, next) => {
   console.log('First middleware');
   next();
 });
