775a23de0c0e58f0ad52dfeadfe46ffee121b84a
TonghuiJiang <jiangt14@mcmaster.ca>
2018-06-25 23:54:47 -0400

    Adding teh POST Backend Point

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -14,12 +18,16 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
 });
 
-app.use('/api/posts', (req, res, next) => {
+app.use("/api/posts", (req, res, next) => {
   const posts = [
     {
       id: "fadf12421l",
